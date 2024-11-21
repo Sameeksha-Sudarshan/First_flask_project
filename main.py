13cb776cf341015a442ac2303b8a4a1ea0eb9d03
Sameeksha_MacbookPro <sameekshasudarshan06@gmail.com>
2024-11-21 21:18:34 +0530

    i added About_me and also added time

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify
 
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify({'message': 'First project!!'})
+    return jsonify({'message': f'The time now is: {datetime.datetime.now()}'})
+
+@app.route('/About_me')
+def About_me():
+    return jsonify({'message': 'I am Sameeksha!'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
